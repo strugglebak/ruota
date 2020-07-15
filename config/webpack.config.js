62adf7fc9f1188d549691a57d3849f6992f71513
strugglebak <strugglebak@gmail.com>
2020-07-15 23:17:45 +0800

    feat: 项目初始化

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: ''
+  entry: {
+    index: path.resolve(__dirname, '../', 'lib/index.tsx')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, '../', 'dist/lib'),
+    library: 'RUOTA',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
 }
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,6 @@
-console.log("hello world")
+import Button from './button';
+
+const a = 1
+console.log('hi')
+
+export {Button, a};
